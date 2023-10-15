7a1ac75335dc0f071de9dad2691d378f3d6bb1c1
racisar <148013983+racisar@users.noreply.github.com>
2023-10-16 01:01:33 +0200

    Update paginas.js

--- JavaScript/paginas.js
+++ JavaScript/paginas.js
@@ -1,9 +1,9 @@
 const book = document.querySelector('.book');
 const pages = document.querySelectorAll('.page');
-let currentPage = 0;
+let currentPage = 1; // Comenzamos en la página 1
 
 function showPage(pageIndex) {
-    if (pageIndex >= 0 && pageIndex < pages.length) {
+    if (pageIndex >= 1 && pageIndex <= pages.length) {
         pages[currentPage].style.transform = 'rotateY(0deg)';
         currentPage = pageIndex;
         pages[currentPage].style.transform = 'rotateY(0deg)';
